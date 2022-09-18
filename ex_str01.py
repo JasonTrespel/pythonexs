2716c3af64dcfc2c150c1ca8941c7d8fd0276a5c
JasonTrespel <84817482+JasonTrespel@users.noreply.github.com>
2022-09-18 19:15:28 -0400

    Update ex_str01.py

--- ex_str01.py
+++ ex_str01.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-# practice with effecting string values
+"""practice with splicing string values"""
+
+# create string
 string1 = "This is my first example!"
 
 # Showing that even strings have position values
@@ -9,12 +11,12 @@ print(string1[5:16])
 # showing strings can be repeated by adding multipliers
 print(string1*3)
 
-# move to using string2 and more examples
+# create second string
 string2 = "This is my second example!"
+# see results from split argument
 string2 = str.split(string2)
 print(string2)
 
-# making everthing CAPITAL
+# MAKING EVERYTHING CAPITAL
 string2 = str.upper(string1)
 print(string2)
-
