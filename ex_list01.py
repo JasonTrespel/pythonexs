11bf514aa77a901e2d0b41a5663136af171f7f12
JasonTrespel <84817482+JasonTrespel@users.noreply.github.com>
2022-09-18 18:49:42 -0400

    Update ex_list01.py

--- ex_list01.py
+++ ex_list01.py
@@ -6,12 +6,18 @@ mylist01 = [1, 2, 3, 4, 5 ,6]
 print(mylist01)
 
 # showing how lists are ordered in python
+# print item 2
 print(mylist01[2])
+# print last item in list
 print(mylist01[-1])
 
 #create new list
 mypetnames = ["Dixie", "Oliver", "Scoots", "Tater Tot", "Rathpberry", "Mr. Barky-von-Schnauzer"]
+
+# show other ways to splice
 print(mypetnames)
 print(mypetnames[0])
+# print list items starting at index value 1
 print(mypetnames[1:])
+# print list items until index value 3
 print(mypetnames[:3])
