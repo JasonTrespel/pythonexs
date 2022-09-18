ff13ed08bde6a36f6524bb55ba82f7fcab392ca5
JasonTrespel <84817482+JasonTrespel@users.noreply.github.com>
2022-09-18 19:19:21 -0400

    Update ex_forloop02.py

--- ex_forloop02.py
+++ ex_forloop02.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+"""creating a more complicated for loop"""
 
+# start with complex list
 cats = [{"name": "garfield", "age": 45}, {"name": "snowball", "age": 7}, {"name": "fluffy", "age": 2}]
 
 print(cats)
@@ -9,8 +11,7 @@ print(cats)
 for cat in cats:
     print(f"My cat named {cat['name']} is {cat['age']} years old.")
 
-# without an f string this is what we would need to print for the same output
+# without an f string this is what we would do to achieve the same output
 print("My cat named garfield is 45 years old.")
 print("My cat named snowball is 7 years old.")
 print("My cat named fluffy is 2 years old.")
-
