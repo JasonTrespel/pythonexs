b5546f752700e09c7d1c4b63f935e5c56f53d43c
JasonTrespel <jtrespel13@gmail.com>
2022-10-03 18:15:52 +0000

    Changes here and there.

--- ex_complexlist.py
+++ ex_complexlist.py
@@ -2,30 +2,35 @@
 
 """create a complex list from a simple list"""
 
-# state simple list and print 
-simplelist = ["oil", "car", "gas"]
-print(simplelist)
-print(simplelist[1])
+def main():
 
-# add new item
-simplelist.append("taxi")
+    # state simple list and print 
+    simplelist = ["oil", "car", "gas"]
+    print(simplelist)
+    print(simplelist[1])
 
-# print out list to see new item
-print(simplelist)
+    # add new item
+    simplelist.append("taxi")
 
-# add new list item to list
-simplelist.append(["cone", "stoplight", "battery"])
+    # print out list to see new item
+    print(simplelist)
 
-# print out extended list
-print(simplelist)
+    # add new list item to list
+    simplelist.append(["cone", "stoplight", "battery"])
 
-# start slicing
-print(simplelist[4][2])
+    # print out extended list
+    print(simplelist)
 
-# add EVEN MORE
-simplelist.append(["green", "blue", 17])
-print(simplelist)
+    # start slicing
+    print(simplelist[4][2])
 
-# MORE SPLICE
-print(simplelist[4][0], simplelist[5])
-print(simplelist[4][0], simplelist[5][2])
+    # add EVEN MORE
+    simplelist.append(["green", "blue", 17])
+    print(simplelist)
+
+    # MORE SPLICE
+    print(simplelist[4][0], simplelist[5])
+    print(simplelist[4][0], simplelist[5][2])
+
+if __name__ == "__main__":
+    main()
