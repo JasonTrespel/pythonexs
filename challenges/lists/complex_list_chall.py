814694f7411ced54dec2a0695dd9c919f15a9803
JasonTrespel <jtrespel13@gmail.com>
2022-10-03 17:01:40 +0000

    more changes made

---
+++ challenges/lists/complex_list_chall.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Create a list of at least five animals with three letter names and have your program output them to the screen."""
+
+# create the main function
+def main():
+
+    # start with creating a list; 5 animals, 3 letter names
+    animal_crossing = ["Cat", "Dog", "Sue", "Zig", "Zag"]
+
+    # print names to screen
+    print(animal_crossing)
+
+
+    for name in animal_crossing:
+        print(name)
+    
+
+    # let's print new names
+    new_list = [new for new in animal_crossing if new.upper().startswith("Z")]
+    print(new_list)
+
+if __name__ == "__main__":
+    main()
