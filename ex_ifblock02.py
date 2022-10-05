7565b872ea501f8a74e78af2f4184f5bdec3501b
JasonTrespel <jtrespel13@gmail.com>
2022-10-05 03:02:12 +0000

    changes here and there.

--- ex_ifblock02.py
+++ ex_ifblock02.py
@@ -1,31 +1,62 @@
 #!/usr/bin/env python3
 
-### Giving letter grades based on number score.
+""" Giving letter grades based on number score."""
+
+# ask user for input
+name = input("What is your name? ")
 
 # set up a statement for explanation
-results = 'Your letter grade is '
+results = 'your letter grade is '
 
-# notice the input has been wrapped within a float function
-score = float(input("What is your score?"))
+cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
 
-# if input is higher than or equal to 90
-if score >= 90:
-    final_results = results + 'A! Amazing job!'
-    
-# if input is higher than or equal to 80
-elif score >= 80:
-    final_results = results + 'B! Great job.'
-    
-# if input is higher than or equal to 70
-elif score >= 70:
-    final_results = results + 'C. Cs get Degrees.'
-    
-# if input is higher than or equal to 60
-elif score >= 60:
-    final_results = results + 'D. Room for improvment.'
-    
-# all other lower integers
-else:
-    final_results = results + 'E. For extra failure.'
+# Set up a while loop to keep asking for user scores.
+while cont == "yes":
 
-print(final_results)
+    # notice the input has been wrapped within a float function
+    score = float(input("What is your score? "))
+    
+    # if input is higher than or equal to 90
+    if score >= 90:
+        final_results = name + ', ' + results + 'A! Amazing job!'
+        print(final_results)
+        cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
+        if cont == "no":
+            print("Oh, alright. Maybe next time.")
+            break
+
+    # if input is higher than or equal to 80
+    elif score >= 80:
+        final_results = name + ', ' + results + 'B! Great job.'
+        print(final_results)
+        cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
+        if cont == "no":
+            print("Oh, alright. Maybe next time.")
+            break
+
+    # if input is higher than or equal to 70
+    elif score >= 70:
+        final_results = name + ', ' + results + 'C. Cs get Degrees.'
+        print(final_results)
+        cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
+        if cont == "no":
+            print("Oh, alright. Maybe next time.")
+            break
+
+    # if input is higher than or equal to 60
+    elif score >= 60:
+        final_results = name + ', ' + results + 'D. Room for improvment.'
+        print(final_results)
+        cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
+        if cont == "no":
+            print("Oh, alright. Maybe next time.")
+            break
+
+    # all other lower integers
+    else:
+        final_results = name + ', ' + results + 'E. For extra failure.'
+        print(final_results)
+        cont = input("Would you like to enter a number score and see your grade? 'yes' or 'no' ")
+        if cont == "no":
+            print("Oh, alright. Maybe next time.")
+            break
