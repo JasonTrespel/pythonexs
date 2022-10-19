1206f0ea64166b19d38ba546ac4560e1c57c71d9
JasonTrespel <84817482+JasonTrespel@users.noreply.github.com>
2022-10-18 22:08:50 -0400

    Update custom_interfaceMAC.py

--- PYNA/custom_interfaceMAC.py
+++ PYNA/custom_interfaceMAC.py
@@ -1,1 +1,15 @@
-Placeholder
+#!/usr/bin/env python3
+  
+import netifaces
+
+print(netifaces.interfaces())
+
+# user needs to choose which adapter to get the MAC addr from
+inter = input("Which interface would you like to see the MAC address? ")
+
+print(netifaces.ifaddresses(inter))
+
+print("\n")
+# let's borrow a line from lab 23
+print("MAC: ", end="")
+print((netifaces.ifaddresses(inter)[netifaces.AF_LINK])[0]["addr"])
