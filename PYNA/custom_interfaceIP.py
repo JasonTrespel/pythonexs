3f5d036db5806def4b8bb25ace097a9733a527ad
JasonTrespel <84817482+JasonTrespel@users.noreply.github.com>
2022-10-18 22:09:42 -0400

    Update custom_interfaceIP.py

--- PYNA/custom_interfaceIP.py
+++ PYNA/custom_interfaceIP.py
@@ -1,1 +1,15 @@
-Placeholder
+#!/usr/bin/env python3
+  
+import netifaces
+
+print(netifaces.interfaces())
+
+# user needs to choose which adapter to get the IP from
+inter = input("Which interface would you like to see the IP address? ")
+
+print(netifaces.ifaddresses(inter))
+
+print("\n")
+# let's borrow a line from lab 23
+print("IP: ", end="")
+print((netifaces.ifaddresses(inter)[netifaces.AF_INET])[0]["addr"])
